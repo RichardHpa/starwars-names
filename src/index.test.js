c32143ef6298be2f3b08d9aa48ad940705f4aaee
RichardHpa <richard.hpa@hotmail.com>
2021-09-18 21:36:05 +1200

    added tests for all and random

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,26 @@ const expect = require('chai').expect
 const starWars = require('./index')
 
 describe('starwars-names', () => {
-	it('should work', () => {
-		expect(true).to.be.true
+	describe('all', () => {
+		it('should be an array of strings', () => {
+			expect(starWars.all).to.satisfy(isArrayOfStrings)
+
+			function isArrayOfStrings(array) {
+				return array.every((item) => {
+					return typeof item === 'string'
+				})
+			}
+		})
+
+		it('should contain `Luke Skywalker`', () => {
+			expect(starWars.all).to.includes('Luke Skywalker')
+		})
+	})
+
+	describe('random', () => {
+		it('should return a random item from starWars.all', () => {
+			const randomItem = starWars.random()
+			expect(starWars.all).to.includes(randomItem)
+		})
 	})
 })
